b45bcda00c44571ed2854625353bdbcb26af3322
TarasovIlia <tarasov002@gmail.com>
2021-09-05 17:42:21 +0300

    add search for products

--- src/app/components/edit-product/product.component.ts
+++ src/app/components/edit-product/product.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../services/api.service'
 import { Products } from "../../models/products"
 
-
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
@@ -10,19 +9,14 @@ import { Products } from "../../models/products"
 })
 export class ProductComponent implements OnInit {
   public products: Products[];
+  public search: string;
     
+  constructor(private apiServer : ApiService) {
+  }
   
-  
-    constructor(private apiServer : ApiService) {
-      
-    }
-  
-    ngOnInit() {
-      this.apiServer.getProduct().subscribe(res => {
-           this.products = res;
-       });
-    
-    }
-  
+  ngOnInit() {
+    this.apiServer.getProduct().subscribe(res => {
+        this.products = res;
+    });
+  }
 }
-
